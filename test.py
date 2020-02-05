a774b895bdcc91aa0b1f58082fde4049a60b87d8
Akinari Hirano <akinari.hirano@pixiedusttech.com>
2020-02-05 10:37:26 +0900

    added pandas

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 x = np.linspace(0,10,100)
 y = np.sin(x)
 
-plt.plot(x,y)
+df = pd.DataFrame({'x':x,'y':y})
+print(df.head())
+
+plt.plot(df.x, df.y)
 plt.show()
 
 
